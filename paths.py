0fac12086f82d26fc23469c7b461fb9c8d2f2666
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-04-19 16:08:56 +0200

    Create paths.py

---
+++ paths.py
@@ -0,0 +1,8 @@
+# URLs
+login_url = "https://web.spaggiari.eu/home/app/default/login.php"
+main_page_url = "https://web.spaggiari.eu/home/app/default/menu_webinfoschool_studenti.php"
+
+# Variables for login
+code_input = "/html/body/div/main/div[1]/div/div/form/div[1]/div[1]/input"
+password_input = "/html/body/div/main/div[1]/div/div/form/div[1]/div[2]/input"
+login_button = "/html/body/div/main/div[1]/div/div/form/button"
