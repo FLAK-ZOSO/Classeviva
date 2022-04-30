29a98d6a883bffa84480c58293bb70f7c3c1c45a
FLAK-ZOSO <83119697+FLAK-ZOSO@users.noreply.github.com>
2022-04-30 22:56:19 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name='classevivaAPI',
+    packages=['classevivaAPI'],
+    package_dir={'': 'src'},
+    version='0.0.7',
+    description='Classeviva Python API',
+    author='FLAK-ZOSO',
+    author_email='mattia.marchese.2006@gmail.com',
+    url='https://github.com/Lioydiano/Classeviva',
+    download_url='https://github.com/Lioydiano/Classeviva/archive/refs/tags/v0.0.7.tar.gz',
+    keywords=['classeviva', 'school', 'api'],
+    classifiers=[],
+)
