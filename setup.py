e183f0a80a3e294f67c6df170fd60b10238e83aa
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-05-01 15:20:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
 import setuptools
 setuptools.setup(
     name='classevivaAPI',
-    packages=['classevivaAPI'],
+    packages=['classeviva'],
     package_dir={'': 'src'},
-    version='0.0.7',
+    version='0.1.0',
     description='Classeviva Python API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='FLAK-ZOSO',
     author_email='mattia.marchese.2006@gmail.com',
     url='https://github.com/Lioydiano/Classeviva',
-    download_url='https://github.com/Lioydiano/Classeviva/archive/refs/tags/v0.0.7.tar.gz',
+    download_url='https://github.com/Lioydiano/Classeviva/archive/refs/tags/v0.1.0.tar.gz',
     keywords=['classeviva', 'school', 'api'],
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
+<<<<<<< Updated upstream
     python_requires=">=3.10"
     # packages=setuptools.find_packages(where="src")
 )
+=======
+    python_requires=">=3.10",
+)
+>>>>>>> Stashed changes
