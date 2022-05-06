65352d3b6b8cab9db9e75b47eb9a6ece3968db75
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-05-06 20:09:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setuptools.setup(
     name='classevivaAPI',
     packages=['classeviva'],
     package_dir={'': 'src'},
-    version='0.1.1.1',
+    version='0.1.2',
     requires=["selenium"],
     description='Classeviva Python API',
     long_description=open('README.md').read(),
@@ -11,13 +11,19 @@ setuptools.setup(
     author='FLAK-ZOSO',
     author_email='mattia.marchese.2006@gmail.com',
     url='https://github.com/Lioydiano/Classeviva',
-    download_url='https://github.com/Lioydiano/Classeviva/archive/refs/tags/v0.1.1-alfa.1.tar.gz',
+    download_url='https://github.com/Lioydiano/Classeviva/archive/refs/tags/v0.1.2.tar.gz',
     keywords=['classeviva', 'school', 'api'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Development Status :: 2 - Pre-Alpha",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Natural Language :: Italian",
+        "Natural Language :: English",
+        "Typing :: Typed"
     ],
     python_requires=">=3.10"
 )
